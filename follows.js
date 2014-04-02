15004ec4d2a20b9c88fa6f677657b8bbc596507c
Samtad <samuel_diarra@ymail.com>
2014-04-02 16:16:14 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -3,7 +3,7 @@
 	/*
 	EXAMPLE CONFIGURATION
 
-		var defaultKey	= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
+		var defaultKey	= 'Ev6yywDQyHhURZcvETHFynZLbE9KRsssozyxVRZxJQp3tLZG', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [61916,12425,94322], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'My Company\'s Application', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
